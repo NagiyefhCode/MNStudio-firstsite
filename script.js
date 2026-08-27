6a32ad53b44a1c893d79483cecb01db4a04319b2
NagiyefhCode <maqanagiyefh@gmail.com>
2026-08-28 00:51:31 +0400

    Fix merge conflict in script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const button = document.getElementById("contactBtn");
 
 button.addEventListener("click", function() {
     alert("Salam! Bizimlə əlaqə saxladınız.");
-=======
-const button = document.getElementById("contactBtn");
-
-button.addEventListener("click", function() {
-    alert("Salam! Bizimlə əlaqə saxladınız.");
->>>>>>> 9b0d675dc4e39f60a83d952926ea6594191e2d5d
 });
