cd750555da52857ec37f6817129dbc8e3bd5ade1
Mehemmed Nagiyev <maqanagiyefh@gmail.coml>
2026-08-29 01:29:26 +0400

    Update contact section

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const button = document.getElementById("contactBtn");
+const contactBtn = document.getElementById("contactBtn");
+const contactModal = document.getElementById("contactModal");
+const closeBtn = document.getElementById("closeBtn");
 
-button.addEventListener("click", function() {
-    alert("Salam! Bizimlə əlaqə saxladınız.");
+contactBtn.addEventListener("click", function() {
+    contactModal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", function() {
+    contactModal.style.display = "none";
+});
+
+contactModal.addEventListener("click", function(event) {
+    if (event.target === contactModal) {
+        contactModal.style.display = "none";
+    }
 });
